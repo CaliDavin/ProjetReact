da8ad9c91e6c355558f4efe4f25428621e742624
Cali Davin <davin.calimouttoupoulle@euro-isolation.com>
2022-07-01 17:14:36 +0200

    main

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,9 +8,11 @@ const Main = () => {
     return (
         <>
             <h1>Bienvenue</h1>
-            <Card title="Villa Ibiza" image={Ibiza} prix="250€/nuit" desc="Magnifique villa à Ibiza avec piscine, 3 chambres et jusqu'à 6 voyageurs, a 5 min du centre-ville et des boîtes de nuit !"  />
-            <Card title="Best villa d'ibiza" image={Ibiza2} prix="190€/nuit" desc="Petite villa au plein centre-ville d'Ibiza, avec une petite piscine et 2 chambres avec salle de bain cette villa et la meilleure de l'île. (4 voyageur Max)"  />
-            <Card title="Best Chambre d'Hôtel d'ibiza" image={Ibiza3} prix="220/nuit" desc="Chambre d'hôtel de luxe avec votre propre piscine privée, Hotel situé en plein centre-ville "  />
+            <div className="row">
+                <Card title="Villa Ibiza" image={Ibiza} prix="250€/nuit" desc="Magnifique villa à Ibiza avec piscine, 3 chambres et jusqu'à 6 voyageurs, a 5 min du centre-ville et des boîtes de nuit !"  />
+                <Card title="Best villa d'ibiza" image={Ibiza2} prix="190€/nuit" desc="Petite villa au plein centre-ville d'Ibiza, avec une petite piscine et 2 chambres avec salle de bain cette villa et la meilleure de l'île. (4 voyageur Max)"  />
+                <Card title="Best Chambre d'Hôtel d'ibiza" image={Ibiza3} prix="220/nuit" desc="Chambre d'hôtel de luxe avec votre propre piscine privée, Hotel situé en plein centre-ville "  />
+            </div>
             <JsxExemple />
         </>
     )
